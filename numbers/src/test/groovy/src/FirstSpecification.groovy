c2d4507ff52ac46994a1915f0cdab2a1bcdb75d2
tomaspm98 <up202006845@fe.up.pt>
2022-10-15 01:37:54 +0100

    Point 11.2

---
+++ numbers/src/test/groovy/src/FirstSpecification.groovy
@@ -0,0 +1,19 @@
+import org.apache.tools.ant.taskdefs.optional.extension.Specification
+
+class FirstSpecification {
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+
+    def "two plus two should equal four"() {
+        given:
+        int left = 2
+        int right = 2
+        when:
+        int result = left + right
+        then:
+        result == 4
+    }
+
+}
